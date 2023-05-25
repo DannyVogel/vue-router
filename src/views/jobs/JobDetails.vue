ecab83098a9b1623cdf7338ba63e4c68d29a4aa9
DannyVogel <danny.hj.vogel@gmail.com>
2023-05-25 16:32:08 +0200

    add fetch data from mock api

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,7 +1,11 @@
 <template>
-    <div>
-        <h1>Job Details</h1>
+    <div v-if="job">
+        <h1>{{ job.title }}</h1>
         <p>The job id is {{ id }}</p>
+        <p>{{ job.details }}</p>
+    </div>
+    <div v-else>
+        <p>Loading...</p>
     </div>
 </template>
 
@@ -10,8 +14,16 @@ export default {
     props: ['id'],
     data() {
         return {
-            
+            job: null
         }
+    },
+    mounted() {
+        fetch('http://localhost:3000/jobs/' + this.id)
+            .then(response => response.json())
+            .then(data => {
+                this.job = data
+            })
+            .catch(error => console.log(error.message))
     }
 }
 
--- src/views/jobs/JobsView.vue
+++ src/views/jobs/JobsView.vue
@@ -1,9 +1,14 @@
 <template>
     <div>
         <h1>Jobs</h1>
-        <div v-for="job in jobs" :key="job.id">
-            <RouterLink :to="{name: 'job-details', params: {id: job.id}}">{{ job.title }}</RouterLink>
-            <p>{{ job.details }}</p>
+        <div v-if="jobs.length">
+            <div v-for="job in jobs" :key="job.id">
+                <RouterLink :to="{name: 'job-details', params: {id: job.id}}">{{ job.title }}</RouterLink>
+                <p>{{ job.details }}</p>
+            </div>
+        </div>
+        <div v-else>
+            <p>Loading...</p>
         </div>
     </div>
 </template>
@@ -13,13 +18,24 @@ export default {
     name: 'JobsView',
     data() {
         return {
-            jobs: [
-                { title: 'Ninja', id: 1, details: 'You will be a ninja' },
-                { title: 'Samurai', id: 2, details: 'You will be a samurai' },
-                { title: 'Ronin', id: 3, details: 'You will be a ronin' }
-            ]
+            // data hardcoded
+            // jobs: [
+            //     { title: 'Ninja', id: 1, details: 'You will be a ninja' },
+            //     { title: 'Samurai', id: 2, details: 'You will be a samurai' },
+            //     { title: 'Ronin', id: 3, details: 'You will be a ronin' }
+            // ]
+            // data fetched from API
+            jobs: []
         }
-    }
+    },
+    mounted() {
+        fetch('http://localhost:3000/jobs')
+            .then(response => response.json())
+            .then(data => {
+                this.jobs = data
+            })
+            .catch(error => console.log(error.message))
+    },
 }
 </script>
 
